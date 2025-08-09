526df22c6fdc4207bf1ca4636a3fe7ec60bcf66c
Juan-C-Ceballos <juancruzceballos0@gmail.com>
2025-08-09 15:53:31 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+const App = () => {
+  
+  return (
+    <div className="bg-zinc-800 h-screen">
+      <h1 className="text-white text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
